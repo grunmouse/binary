8b354a0cbf867c604419d7b5f4718c223083540d
Dmitry Rakov <rakov@mirapolis.ru>
2020-10-22 04:15:49 +0300

    init

---
+++ index.js
@@ -0,0 +1,95 @@
+
+const mask = (size)=>((1n<<BigInt(size))-1n);
+
+const {top, vilka, find} = requre('./service.js');
+
+function over2(value){
+	let [a, b] = find((x)=>(value>>x), 1n, (x)=>(x<<1n));
+	
+	if(value === 1n<<a){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
+function ilog2(value){
+	let [a, b] = find((x)=>(value>>x), 1n, (x)=>(x<<1n));
+	return b-1;
+}
+
+/**
+ * Подсчитывает нулевые биты в младших разрядах до первой единицы, возвращает их количество
+ * @param {BigInt} value
+ * @return {BigInt}
+ */
+function lowerZeroCount(value){
+	if(value & 1n === 1n){
+		return 0n;
+	}
+	if(value & 2n === 1n){
+		return 1n
+	}
+	
+	let [a, b] = find((x)=>(value & mask(x) === 0), 1n, (x)=>(x<<1n));
+	return a;
+}
+
+/**
+ * Подсчитывает единичные биты в младших разрядах до первого нуля, возвращает их количество
+ * @param {BigInt} value
+ * @return {BigInt}
+ */
+function lowerFlagCount(value){
+	if(value & 1n === 0n){
+		return 0n;
+	}
+	if(value & 2n === 0n){
+		return 1n
+	}
+	let [a, b] = find((x)=>{
+		let m = mask(x);
+		return value & m === m;
+	}, 1n, (x)=>(x<<1n));
+	
+	return a;
+}
+
+
+/**
+ * Все единичные биты в числе, возвращает их количество
+ * @param {BigInt} value 
+ * @return {BigInt}
+ */
+function flagCount(value){
+	let r =0n;
+	while(value){
+		if(value & 1n){
+			++r;
+		}
+		value = value>>>1n;
+	}
+	return r;
+}
+
+/**
+ * Для числа value возвращает ближайшее большее число с тем же количеством единичных битов
+ * @param {BigInt} value
+ * @return {BigInt}
+ */
+function nextEqFlag(value){
+	let lz = lowerZeroCount(value);
+	let f = lowerFlagCount(value>>>lz)-1n;
+	return value + (1n<<lz) + ((1n<<f)-1n);
+}
+
+
+module.exports = {
+	over2,
+	ilog2,
+	lowerZeroCount,
+	lowerFlagCount,
+	flagCount,
+	nextEqFlag
+};
---
+++ service.js
@@ -0,0 +1,56 @@
+/**
+ * Сужает вилку a,b при условии b-a>=1 && fun(a) && !fun(b)
+ * @param Function<BigInt, Boolean> fun
+ * @param BigInt a - нижняя граница вилки, такая, что fun(a)==true
+ * @param BigInt b - верхняя граница вилки, такая, что b>a && fun(b)==false
+ * @return [a, b] - предельно узкая вилка
+ */
+function vilka(fun, a, b){
+	let m;
+	while((b-a)>1n){
+		m = (a+b)>>1n;
+		if(fun(m)){
+			a = m;
+		}
+		else{
+			b = m;
+		}
+	}
+	return [a, b];
+}
+
+
+/**
+ * Находит верхний предел области поиска, возвращает первую вилку
+ * @param Function<BigInt, Boolean> fun
+ * @param BigInt a - начальное значение нижнего предела
+ * @param Function<BigInt, BigInt> next - функция увеличения значения
+ * @return {a, b} - начальная вилка
+ */
+function top(fun, a, next){
+	let m, b;
+	while(!b){
+		m = next(a);
+		if(fun(m)){
+			a = m;
+		}
+		else{
+			b = n;
+		}
+	}
+	return [a, b];
+}
+
+function find(fun, a, next){
+	let b;
+	[a, b] = top(fun, a, next);
+	[a, b] = vilka(fun, a, b);
+	
+	return [a, b];
+}
+
+module.exports = {
+	vilka,
+	top,
+	find
+}
