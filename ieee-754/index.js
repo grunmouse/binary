9ab026f6717e8aacd2db020ab98fe568a202fb37
Dmitry Rakov <rakov@mirapolis.ru>
2023-06-30 03:38:44 +0300

    debug

--- ieee-754/index.js
+++ ieee-754/index.js
@@ -5,13 +5,17 @@ const accuracy = require('./accuracy.js')
 const {
 	decompFloat64,
 	packFloat64,
-	makeFloat64
+	makeFloat64,
+	fromIndex,
+	getIndex,
 } = require('./decomp-number.js');
 
 module.exports = {
 	decomp:decompFloat64,
 	pack:packFloat64,
 	make:makeFloat64,
+	fromIndex,
+	getIndex,
 	...require('./ilog2.js'),
 	round,
 	accuracy
